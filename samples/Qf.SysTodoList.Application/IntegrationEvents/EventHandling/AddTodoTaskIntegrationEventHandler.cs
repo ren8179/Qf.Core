f4e336a7ca05f56c414b18086492f848368e00a2
renqi <ren8179@126.com>
2020-01-02 18:10:07 +0800

    添加集成事件支持

---
+++ samples/Qf.SysTodoList.Application/IntegrationEvents/EventHandling/AddTodoTaskIntegrationEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using DotNetCore.CAP;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Qf.SysTodoList.Application.Commands;
+using Qf.SysTodoList.Application.IntegrationEvents.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qf.SysTodoList.Application.IntegrationEvents.EventHandling
+{
+    public class AddTodoTaskIntegrationEventHandler : ICapSubscribe
+    {
+        private readonly ILogger<AddTodoTaskIntegrationEventHandler> _logger;
+        private readonly IMediator _mediator;
+
+        public AddTodoTaskIntegrationEventHandler(
+            ILogger<AddTodoTaskIntegrationEventHandler> logger,
+            IMediator mediator)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [CapSubscribe(nameof(AddTodoTaskIntegrationEvent))]
+        public async Task Handle(AddTodoTaskIntegrationEvent @event)
+        {
+            await _mediator.Send(new CreateTodoTaskCommand(@event.Data));
+        }
+    }
+}
---
+++ samples/Qf.SysTodoList.Application/IntegrationEvents/Events/AddTodoTaskIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿using Qf.SysTodoList.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qf.SysTodoList.Application.IntegrationEvents.Events
+{
+    public class AddTodoTaskIntegrationEvent
+    {
+        public CreateTodoTaskInput Data { get; set; }
+
+        public AddTodoTaskIntegrationEvent(CreateTodoTaskInput data)
+        {
+            Data = data;
+        }
+    }
+}
