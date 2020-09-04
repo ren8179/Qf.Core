017e2f8fade03ab5aea1d136f166aea542786c86
renqi <ren8179@126.com>
2020-09-04 18:43:01 +0800

    通过网关Ocelot,获取到客户端IP

---
+++ samples/Qf.SysTodoList.WebApi/Controllers/ToolsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Qf.SysTodoList.WebApi.Controllers
+{
+    [ApiController]
+    [Route(Program.AppName + "/[controller]")]
+    public class ToolsController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolsController"/> class.
+        /// </summary>
+        public ToolsController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        /// <summary>
+        /// 通过网关Ocelot,获取到客户端IP
+        /// </summary>
+        [HttpGet("getClientIP")]
+        [AllowAnonymous]
+        public IActionResult GetClientIP()
+        {
+            var clientIP = _httpContextAccessor.HttpContext.Request.Headers["X-Forwarded-For"];
+            return Ok(clientIP);
+        }
+    }
+}
